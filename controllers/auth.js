ac67746974f4f27aaa607e5b26af905564314eff
Jhonny Martínez <jmartinezespza@gmail.com>
2021-10-22 00:26:39 -0500

    Rest-api con Google SignIn

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
 
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async ( req, res = response ) => {
 
@@ -53,6 +54,55 @@ const login = async ( req, res = response ) => {
 
 }
 
+const googleSignIn = async( req, res = response ) => {
+    const { id_token } = req.body;
+
+    try {
+        const { nombre, img, correo } = await googleVerify( id_token );
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+            };
+
+            usuario = new Usuario( data );
+            
+            await usuario.save();
+        }
+        console.log(nombre)
+
+        // Si el usuario en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el Administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            error: error,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
